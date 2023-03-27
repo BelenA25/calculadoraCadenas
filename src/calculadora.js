35bdfca79a4023a2926622b05efcdea4292c8430
“Belen <beckiar23@gmail.com>
2023-03-27 19:50:22 -0400

    Refactoring - añadir funcion para comprobar tamaño de cadena

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,29 +1,31 @@
 function calculadora(cadena) {
-  if (cadena == "") {
+  if (tamanoCadena(cadena)==0) {
     return 0;
   }
   else {
-    if (cadena.length > 1) {
+    if (tamanoCadena(cadena) > 1) {
       if (cadena[0] == "/") {
         let cadenas = cadena.split(" ");
         let delimitador = cadenas[0][3];
         let delimitadores = "/,|-|;/";
-        // let cadenaNumeros = conversion(cadenas[1].split(delimitadores));
-        let cadenaNumeros = conversion(splitMulti(cadenas[1], [',','-',delimitador]));
+        let cadenaNumeros = conversion(splitMulti(cadenas[1], [',', '-', delimitador]));
         return sumar(cadenaNumeros);
       }
       else {
         let cadenaNumeros = conversion(cadena.split(/,|-/));
         return sumar(cadenaNumeros);
       }
-
     }
     else {
       return parseInt(cadena);
     }
   }
-
 }
+
+function tamanoCadena(cadena){
+  return cadena.length;
+}
+
 function sumar(numeros) {
   let suma = 0;
   for (let i = 0; i < numeros.length; i++) {
@@ -31,6 +33,7 @@ function sumar(numeros) {
   }
   return suma;
 }
+
 function conversion(cadena) {
   let nuevaCadena = [];
   for (let i = 0; i < cadena.length; i++) {
@@ -38,6 +41,7 @@ function conversion(cadena) {
   }
   return nuevaCadena;
 }
+
 function splitMulti(str, tokens) {
   var tempChar = tokens[0];
   for (var i = 1; i < tokens.length; i++) {
@@ -46,4 +50,5 @@ function splitMulti(str, tokens) {
   str = str.split(tempChar);
   return str;
 }
+
 export default calculadora;
